4883e6f9a7de5f6a59174423ca87644a1cb956d5
Alejandro <836445882@qq.com>
2019-12-27 16:45:44 +0800

    五

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60229" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60925" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -323,4 +323,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["C:/Users/Han/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+//# sourceMappingURL=dist/main.1f19ae8e.js.map
